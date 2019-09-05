4614a97802fa233102b3c9aa807f39f4ecffa395
Koushik Saha <Koushik.saha666@gmail.com>
2019-09-05 10:29:25 +0600

    Add app bar layout along with Add Up Button and Back Button on Toolbar on Post Activity

--- app/src/main/java/codingcafe/example/com/PostActivity.java
+++ app/src/main/java/codingcafe/example/com/PostActivity.java
@@ -2,14 +2,48 @@ package codingcafe.example.com;
 
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class PostActivity extends AppCompatActivity {
+public class PostActivity extends AppCompatActivity
+{
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+
+        mToolbar = (Toolbar) findViewById(R.id.update_post_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Update Post");
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if(id == android.R.id.home)
+        {
+            SendUserToMainActivity();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void SendUserToMainActivity()
+    {
+        Intent mainIntent = new Intent(PostActivity.this, MainActivity.class);
+        startActivity(mainIntent);
+    }
+
 }
