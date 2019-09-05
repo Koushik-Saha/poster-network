00b86a5ba228c18931d9ed23e090f2e2ddc31f94
Koushik Saha <Koushik.saha666@gmail.com>
2019-09-05 11:11:23 +0600

    Allow the user to select image from his gallery to update a new post

--- app/src/main/java/codingcafe/example/com/PostActivity.java
+++ app/src/main/java/codingcafe/example/com/PostActivity.java
@@ -7,10 +7,23 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class PostActivity extends AppCompatActivity
 {
     private Toolbar mToolbar;
+
+    private ImageButton SelectPostImage;
+    private Button UpdatePostButton;
+    private EditText PostDescription;
+
+
+    private static final int Gallery_Pick = 1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +31,36 @@ public class PostActivity extends AppCompatActivity
         setContentView(R.layout.activity_post);
 
 
+
+        SelectPostImage = (ImageButton) findViewById(R.id.select_post_image);
+        UpdatePostButton = (Button) findViewById(R.id.update_post_button);
+        PostDescription =(EditText) findViewById(R.id.post_description);
+
+
         mToolbar = (Toolbar) findViewById(R.id.update_post_page_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("Update Post");
+
+
+        SelectPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                OpenGallery();
+            }
+        });
+    }
+
+
+
+    private void OpenGallery()
+    {
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, Gallery_Pick);
     }
 
 
