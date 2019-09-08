3bb31bc7c23c2ade50a681f05af224a5d6a0861d
Koushik Saha <Koushik.saha666@gmail.com>
2019-09-08 12:19:41 +0600

    Retrieve data from firebase database and display it on profile activity

--- app/src/main/java/codingcafe/example/com/MainActivity.java
+++ app/src/main/java/codingcafe/example/com/MainActivity.java
@@ -329,6 +329,7 @@ public class MainActivity extends AppCompatActivity
                 break;
 
             case R.id.nav_profile:
+                SendUserToProfileActivity();
                 Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
                 break;
 
@@ -364,6 +365,12 @@ public class MainActivity extends AppCompatActivity
     private void SendUserToSettingsActivity()
     {
         Intent loginIntent = new Intent(MainActivity.this, SettingsActivity.class);
+        startActivity(loginIntent);
+    }
+
+    private void SendUserToProfileActivity()
+    {
+        Intent loginIntent = new Intent(MainActivity.this, ProfileActivity.class);
         startActivity(loginIntent);
     }
 }
--- app/src/main/java/codingcafe/example/com/ProfileActivity.java
+++ app/src/main/java/codingcafe/example/com/ProfileActivity.java
@@ -6,6 +6,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity
@@ -14,10 +22,19 @@ public class ProfileActivity extends AppCompatActivity
     private TextView userName, userProfName, userStatus, userCountry, userGender, userRelation, userDOB;
     private CircleImageView userProfileImage;
 
+    private DatabaseReference porfileUserRef;
+    private FirebaseAuth mAuth;
+
+    private String currentUserId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserId = mAuth.getCurrentUser().getUid();
+        porfileUserRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserId);
 
 
         userName = (TextView) findViewById(R.id.my_username);
@@ -28,5 +45,43 @@ public class ProfileActivity extends AppCompatActivity
         userRelation = (TextView) findViewById(R.id.my_relationship_status);
         userDOB = (TextView) findViewById(R.id.my_dob);
         userProfileImage = (CircleImageView) findViewById(R.id.my_profile_pic);
+
+        porfileUserRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                if (dataSnapshot.exists())
+                {
+                    String myProfileImage = dataSnapshot.child("profileimage").getValue().toString();
+                    String myUserName = dataSnapshot.child("username").getValue().toString();
+                    String myProfileName = dataSnapshot.child("fullname").getValue().toString();
+                    String myProfileStatus = dataSnapshot.child("status").getValue().toString();
+                    String myDOB = dataSnapshot.child("dob").getValue().toString();
+                    String myCountry = dataSnapshot.child("country").getValue().toString();
+                    String myGender = dataSnapshot.child("gender").getValue().toString();
+                    String myRelationStatus = dataSnapshot.child("relationshipstatus").getValue().toString();
+
+
+                    Picasso.with(ProfileActivity.this).load(myProfileImage).placeholder(R.drawable.profile).into(userProfileImage);
+
+
+                    userName.setText("@" + myUserName);
+                    userProfName.setText(myProfileName);
+                    userStatus.setText(myProfileStatus);
+                    userDOB.setText("DOB : " + myDOB);
+                    userCountry.setText("Country : " + myCountry);
+                    userGender.setText("Gender : " + myGender);
+                    userRelation.setText("Relationship : " + myRelationStatus);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError)
+            {
+
+
+            }
+        });
     }
 }
