c0b8bcd3d0adec4fa155df3e2b4f652dd74bd2d6
Koushik Saha <Koushik.saha666@gmail.com>
2019-09-11 01:29:10 +0600

    Use firebase recyclerview adapter with firebase recyclerview for displaying all Comments from firebase database

--- app/src/main/java/codingcafe/example/com/CommentsActivity.java
+++ app/src/main/java/codingcafe/example/com/CommentsActivity.java
@@ -11,8 +11,10 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -88,6 +90,80 @@ public class CommentsActivity extends AppCompatActivity
                 });
             }
         });
+    }
+
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<Comments, CommentsViewHolder> firebaseRecyclerAdapter
+                = new FirebaseRecyclerAdapter<Comments, CommentsViewHolder>
+                (
+                        Comments.class,
+                        R.layout.all_comments_layout,
+                        CommentsViewHolder.class,
+                        PostsRef
+
+                )
+        {
+            @Override
+            protected void populateViewHolder(CommentsViewHolder viewHolder, Comments model, int position)
+            {
+                viewHolder.setUsername(model.getUsername());
+                viewHolder.setComment(model.getComment());
+                viewHolder.setDate(model.getDate());
+                viewHolder.setTime(model.getTime());
+
+
+            }
+        };
+
+        CommentsList.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    public static class CommentsViewHolder extends RecyclerView.ViewHolder
+    {
+        View mView;
+
+        public CommentsViewHolder(View itemView)
+        {
+            super(itemView);
+
+            mView = itemView;
+
+        }
+
+        public void setUsername(String username)
+        {
+            TextView myUserName = (TextView) mView.findViewById(R.id.comment_username);
+            myUserName.setText("@"+username+"  ");
+        }
+
+        public void setComment(String comment)
+        {
+            TextView myComment = (TextView) mView.findViewById(R.id.comment_text);
+            myComment.setText(comment);
+
+        }
+
+        public void setDate(String date)
+        {
+            TextView myDate = (TextView) mView.findViewById(R.id.comment_date);
+            myDate.setText("   Date "+date);
+
+        }
+
+        public void setTime(String time)
+        {
+
+            TextView myTime = (TextView) mView.findViewById(R.id.comment_time);
+            myTime.setText("Time: "+time);
+
+        }
+
+
     }
 
     private void ValidateComment(String userName)
