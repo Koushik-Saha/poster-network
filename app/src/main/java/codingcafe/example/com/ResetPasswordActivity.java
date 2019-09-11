53ede879254ac09d5819526b995f924bfc930080
Koushik Saha <Koushik.saha666@gmail.com>
2019-09-11 11:10:32 +0600

    Add firebase reset password functionality and implement the Forgot Password functionality now user will be able to reset password if he forgot password.

--- app/src/main/java/codingcafe/example/com/ResetPasswordActivity.java
+++ app/src/main/java/codingcafe/example/com/ResetPasswordActivity.java
@@ -1,11 +1,20 @@
 package codingcafe.example.com;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ResetPasswordActivity extends AppCompatActivity
 {
@@ -14,11 +23,16 @@ public class ResetPasswordActivity extends AppCompatActivity
     private Button ResetPasswordSendEmailButton;
     private EditText ResetEmailInput;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+
+
+        mAuth = FirebaseAuth.getInstance();
 
         mToolbar = (Toolbar) findViewById(R.id.forget_password_toolbar);
         setSupportActionBar(mToolbar);
@@ -28,5 +42,39 @@ public class ResetPasswordActivity extends AppCompatActivity
 
         ResetPasswordSendEmailButton = (Button) findViewById(R.id.reset_password_email_button);
         ResetEmailInput = (EditText) findViewById(R.id.reset_password_email);
+
+
+        ResetPasswordSendEmailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                String userEmail = ResetEmailInput.getText().toString();
+                
+                if (TextUtils.isEmpty(userEmail))
+                {
+                    Toast.makeText(ResetPasswordActivity.this, "Please write your valid email address first", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    mAuth.sendPasswordResetEmail(userEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task)
+                        {
+                            if (task.isSuccessful())
+                            {
+                                Toast.makeText(ResetPasswordActivity.this, "Please check your Email account for password reset.", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(ResetPasswordActivity.this, LoginActivity.class));
+                            }
+                            else 
+                            {
+                                String message = task.getException().getMessage();
+                                Toast.makeText(ResetPasswordActivity.this, "Error Occurred: " + message, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
     }
 }
