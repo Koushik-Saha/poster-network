4a3da105b28e233d7a26d4f00764e1baacb504de
Koushik Saha <Koushik.saha666@gmail.com>
2019-09-11 11:53:15 +0600

    Create Person Profile Activity and creating Send Friend Request and Decline/Cancel Friend Request Buttons.

---
+++ app/src/main/java/codingcafe/example/com/PersonProfileActivity.java
@@ -0,0 +1,15 @@
+package codingcafe.example.com;
+
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PersonProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_profile);
+    }
+}
