9ad9b59ba692da23b85c39726d86d0e9b0dbf628
Koushik Saha <Koushik.saha666@gmail.com>
2019-09-12 20:09:01 +0600

    Android toolbar of MyPost Activity with back button

--- app/src/main/java/codingcafe/example/com/MyPostsActivity.java
+++ app/src/main/java/codingcafe/example/com/MyPostsActivity.java
@@ -3,12 +3,23 @@ package codingcafe.example.com;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class MyPostsActivity extends AppCompatActivity {
+public class MyPostsActivity extends AppCompatActivity
+{
+
+    private Toolbar mToolbar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_posts);
+
+        mToolbar = (Toolbar) findViewById(R.id.my_post_bar_layout);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("My Posts");
     }
 }
